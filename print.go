2e6e8b0ae3d805e94d1b66ae36d73073991afeb3
BIJIL VARGHESE <varghesebijil1313@gmail.com>
2021-06-04 21:28:53 +0530

    print numbers from 1 to 50

---
+++ print.go
@@ -0,0 +1,13 @@
+package main
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("numbers from 1 to 50:")
+
+	//looping from 1 to 50 and print each no.
+	for i := 1; i <= 50; i++ {
+	   fmt.Printf("%v \n",i)
+	}
+}
